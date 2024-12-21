4bec63f9a4ee2b1cc679933fa76e8766ac041e97
Kunalsahuji <ksahu0103@gmail.com>
2024-12-21 18:09:00 +0530

    create property services

---
+++ frontend/src/api/propertyService.jsx
@@ -0,0 +1,56 @@
+import axios from './axiosConfig'
+import { toast } from 'react-toastify'
+
+export const createPropertyService = async (propertyData) => {
+    try {
+        const { data } = await axios.post('/property/create', propertyData)
+        return data
+    } catch (error) {
+        toast.error(error.response.data.message)
+    }
+}
+
+export const updatePropertyService = async (propertyData, id) => {
+    try {
+        const { data } = await axios.put(`/property/update/${id}`, propertyData)
+        return data
+    } catch (error) {
+        toast.error(error.response.data.message)
+    }
+}
+
+export const deletePropertyServie = async (propertyId) => {
+    try {
+        const { data } = await axios.delete(`/property/delete/${propertyId}`)
+        return data
+    } catch (error) {
+        toast.error(error.response.data.message)
+    }
+}
+
+export const searchPropertiesService = async (query) => {
+    try {
+        const { data } = await axios.get(`/property/search/${query}`)
+        return data
+    } catch (error) {
+        toast.error(error.response.data.message)
+    }
+}
+
+export const searchMyPorpertiesServicec = async () => {
+    try {
+        const { data } = await axios.get('/property/find')
+        return data
+    } catch (error) {
+        toast.error(error.response.data.mesaage)
+    }
+}
+
+export const viewPropertiesService = async (id) => {
+    try {
+        const { data } = await axios.get(`/property/view/${id}`)
+        return data
+    } catch (error) {
+        toast.error(error.response.data.message)
+    }
+}
